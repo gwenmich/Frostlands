b16262b39804676029f93578b307a1fa9f69f7f3
gwenmich <79330147+cgmich@users.noreply.github.com>
2024-12-31 22:47:56 +0000

    Snowball movement logic

--- code/snowball.py
+++ code/snowball.py
@@ -13,13 +13,28 @@ class Snowball(pygame.sprite.Sprite):
         self.created_time = time.time()
         self.lifespan = 0.4
 
+        self.speed = 10
+        self.direction = pygame.math.Vector2()
+
         # placement
         if direction == "right":
             self.rect = self.image.get_rect(midleft = player.rect.midright)
+            self.direction.x = 1
         elif direction == "left":
             self.rect = self.image.get_rect(midright = player.rect.midleft)
+            self.direction.x = -1
         elif direction == "up":
             self.rect = self.image.get_rect(midbottom = player.rect.midtop)
+            self.direction.y = -1
         elif direction == "down":
             self.rect = self.image.get_rect(midtop = player.rect.midbottom)
+            self.direction.y = 1
+
+
+    def update(self):
+        self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y * self.speed
+
+        if time.time() - self.created_time > self.lifespan:
+            self.kill()
 
